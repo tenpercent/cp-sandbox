77993fc8e0a45e38da1377ca627878a8f96e52e7
Max Podkorytov <max.podkorytoff@yandex.ru>
2014-04-04 19:46:06 +0400

    continue problem construction

--- src/BI/diff/thermoconductivity.f90
+++ src/BI/diff/thermoconductivity.f90
@@ -1,5 +1,10 @@
       program thermoconductivity
+        implicit none
 
-
+        include 'mmax.fd'
+        integer, parameter :: namax1 = 4 * namax,
+     &                        nvmax1 = 2 * nvmax ! dlya A1,JA1
+        integer, parameter :: max_work_memory = 8000000
+        integer, dimension (1 : max_work_memory) :: iW
+        
       end program thermoconductivity
-      
