abe3da25f80926ca1d85cb231ada374d8ad3b8c7
Max Podkorytov <max.podkorytoff@yandex.ru>
2014-05-04 20:51:50 +0400

    logic complete

--- src/BI/diff/dirac.f
+++ src/BI/diff/dirac.f
@@ -1,95 +1,46 @@
-      program test_dirac
-        implicit none
-        integer, parameter :: max_tetrahedra = 400000,                                    ! "ntmax"
-     &                        max_mesh_nodes = 70000,                                     ! "nvmax"
-     &                        max_boundary_faces = 5000,                                  ! "nbmax"
-     &                        max_nonzero_matrix_entries = 1000000                        ! "namax"
-        integer, parameter :: max_work_memory_size = 8000000                              ! "MaxWi"
-        integer, dimension (1 : max_work_memory_size) :: work_buffer                      ! "iW"
+      function det2 (x11, x12, x21, x22)
+      implicit none
 
-        integer :: total_mesh_nodes,                                                      ! "nv"
-     &             total_tetrahedra,                                                      ! "nt"
-     &             total_boundary_faces                                                   ! "nb"
+          real*8 :: x11, x12, x21, x22, det2
 
-        real*8, dimension (1 : 3, 1 : max_mesh_nodes) :: 
-     &             node_coordinates                                                       ! "vrt"
-        integer :: node_material_labels                                                   ! "labelP"
+          det2 = x11 * x22 - x21 * x12
 
-        integer, dimension (1 : 4, 1 : max_tetrahedra) ::
-     &             tetrahedra_nodes                                                       ! "tet"
-        integer, dimension (1 : max_tetrahedra) :: 
-     &             tetrahedra_material_labels                                             ! "material"
+      return
+      end function det2
+c====================================================================
+      function det4 (x11, x12, x13, x14,
+     &               x21, x22, x23, x24,
+     &               x31, x32, x33, x34,
+     &               x41, x42, x43, x44)
+      implicit none
 
-        integer, dimension (1 : 3, 1 : max_boundary_faces) :: face_nodes                  ! "bnd"
-        integer, dimension (1 : max_boundary_faces) :: 
-     &             face_material_labels                                                   ! "labelF"
+          real*8 :: x11, x12, x13, x14,
+     &              x21, x22, x23, x24,
+     &              x31, x32, x33, x34,
+     &              x41, x42, x43, x44
 
-        integer, parameter :: max_fixed_points = 9,                                       ! "maxPV"
-     &                        max_fixed_faces = 1,                                        ! "maxFV"
-     &                        max_fixed_elements = 1                                      ! "maxEV"
+          real*8 :: det4, det2
 
-        integer :: total_fixed_points,                                                    ! "nPv"
-     &             total_fixed_faces,                                                     ! "nFv"
-     &             total_fixed_elements                                                   ! "nEv"
-        integer, dimension (1 : max_fixed_points)   :: fixed_points                       ! "IPV"
-        integer, dimension (1 : max_fixed_faces)    :: fixed_faces                        ! "IFV"
-        integer, dimension (1 : max_fixed_elements) :: fixed_elements                     ! "IEV"
-C === AniFEM stuff        
-        include 'fem3Dtet.fd'
-        include 'assemble.fd'
-C === "sr" means "sparse row"
-        integer, dimension (1 : max_mesh_nodes) :: 
-     &             sr_row_nonzero_counter                                                 ! "JA"
-        integer, dimension (1 : max_nonzero_matrix_entries) :: 
-     &             sr_nonzero_entries_columns                                             ! "IA"
-        real*8, dimension (1 : max_nonzero_matrix_entries) :: 
-     &             sr_nonzero_entries                                                     ! "A"
-C === "sle" means "system of linear equations"
-        real*8, dimension (1 : max_mesh_nodes) :: sle_right_hand_side                     ! "RHS"
-        real*8, dimension (1 : max_mesh_nodes) :: sle_solution                            ! "SOL"
-        integer :: total_columns, total_rows                                              ! "nCol", "nRow"
-        integer :: matrix_type                                                            ! "status"
-C === seems that these are arrays of coefficients to be changed
-        real*8, dimension (1 : 44) :: DATAFEM_R                                           ! "DATAFEMR"
-        real*8, dimension (1 : 1) :: DATAFEM_E                                            ! "DATAFEME"
-c === external procedures block
-        external loadMani
+          det4 = det2 (x11, x12, x21, x22) * det2 (x33, x34, x43, x44) -
+     &           det2 (x13, x14, x23, x24) * det2 (x31, x32, x41, x42)
 
-        real*8, dimension (1 : 3) :: point_coordinates
-        data point_coordinates /5d-1, 5d-1, 5d-1/
-        real*8 :: dirac_value, delta_dirac
+      return
+      end function det4
+c====================================================================
+      function tetrahedron_volume (p1, p2, p3, p4)
+      implicit none
 
-        call loadMani(
-     &                max_mesh_nodes, 
-     &                max_boundary_faces, 
-     &                max_tetrahedra,
-     &                total_mesh_nodes, 
-     &                total_boundary_faces, 
-     &                total_tetrahedra,
-     &                node_coordinates, 
-     &                face_nodes, 
-     &                tetrahedra_nodes, 
-     &                face_material_labels, 
-     &                tetrahedra_material_labels,
-     &                total_fixed_points, 
-     &                total_fixed_faces, 
-     &                total_fixed_elements, 
-     &                fixed_points, 
-     &                fixed_faces, 
-     &                fixed_elements,
-     &                work_buffer, 
-     &                work_buffer, 
-     &                "../../../../data/cube.ani")
+          real*8, dimension(1:3) :: p1, p2, p3, p4
+          real*8 :: tetrahedron_volume, det4
 
-        dirac_value = delta_dirac(tetrahedra_nodes, 
-     &                     total_tetrahedra, 
-     &                     node_coordinates, 
-     &                     total_mesh_nodes, 
-     &                     point_coordinates)
+          tetrahedron_volume = abs (det4 (1d0, p1(1), p1(2), p1(3),
+     &                               1d0, p2(1), p2(2), p2(3),
+     &                               1d0, p3(1), p3(2), p3(3),
+     &                               1d0, p4(1), p4(2), p4(3)) / 6d0)
 
-        write (*,*) dirac_value
-
-      end program test_dirac
+      return
+      end function tetrahedron_volume
+c====================================================================
 c==========================================================================================
       function euclid_distance (p1, p2)
       implicit none
@@ -139,7 +90,8 @@ c====================================================================
      &                     total_tetrahedra, 
      &                     node_coordinates, 
      &                     total_mesh_nodes, 
-     &                     point_coordinates)
+     &                     point_coordinates,
+     &                     nearest_node_coordinates)
       implicit none
 
         integer :: total_tetrahedra, total_mesh_nodes
@@ -147,7 +99,8 @@ c====================================================================
      &      tetrahedra_nodes
         real*8, dimension (1 : 3, 1 : total_mesh_nodes) ::
      &      node_coordinates
-        real*8, dimension (1 : 3) :: point_coordinates
+        real*8, dimension (1 : 3) :: point_coordinates, 
+     &      nearest_node_coordinates
         real*8 :: delta_dirac
 
         integer :: nearest_node, search_nearest
@@ -167,22 +120,14 @@ c====================================================================
         nearest_node = search_nearest(node_coordinates, 
      &                         total_mesh_nodes, 
      &                         point_coordinates)
-c === check if nearest_node is not 0
-        write (*, *) nearest_node
+        nearest_node_coordinates = 
+     &    node_coordinates(1 : 3, nearest_node)
 
         call backReferences (total_mesh_nodes, 
      &                       total_tetrahedra, 
      &                       L, M, tetrahedra_nodes, nEP, IEP) 
 
-c === check if tetrahedra_nodes is not empty
-c        do i = 1, total_tetrahedra
-c          write (*,*) tetrahedra_nodes(1:4, i)
-c        end do
-c        write (*,*) nEP(1 : total_mesh_nodes)
-
         do i = nEP(nearest_node - 1) + 1, nEP(nearest_node)
-c === print tetrahedra which contain nearest node; fail
-            write (*, *) tetrahedra_nodes(1 : 4, IEP(i))
             volume = volume + abs(
      &        calVol (
      &          node_coordinates(1 : 3, tetrahedra_nodes(1, IEP(i))), 
